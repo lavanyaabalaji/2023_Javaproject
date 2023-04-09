232572a3abb9f41f16c584aff1b83fa1d4293946
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-09 18:17:16 +0530

    Day02
    Data types and class

--- src/day02/Program1.java
+++ src/day02/Program1.java
@@ -3,8 +3,46 @@ package day02;
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean a=true;
+		System.out.println(a);
+		a=false;
+		System.out.println(a);
+		byte a1=125;
+		System.out.println(a1);
+		
+		short a2=32767;
+		System.out.println(a2);
+		
+		int a3=2345678;
+		System.out.println(a3);
+		
+		a3=a2;
+		System.out.println(a3);
+		
+		long a4=1234567;
+		System.out.println(a4);
+		
+		a4=1234567890456l;
+		System.out.println(a4);
+		
+		float a5=3456789.8145f;
+		System.out.println(a5);
+		
+		double a6=45678.1234567d;
+		System.out.println(a6);
+		
+				
+		
+		
+		
+		
+		
+		
+	
+				
+		
 
+		
 	}
 
 }
