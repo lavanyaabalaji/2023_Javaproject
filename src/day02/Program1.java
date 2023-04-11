072a92b53e9ba70c774ee572e52b47dcc5dbda5d
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-11 19:02:21 +0530

    Day03
    Mainly   commit,static/Non-static

--- src/day02/Program1.java
+++ src/day02/Program1.java
@@ -1,45 +1,48 @@
 package day02;
 
 public class Program1{
-
-	public static void main(String[] args) {
-		boolean a=true;
-		System.out.println(a);
-		a=false;
-		System.out.println(a);
-		byte a1=125;
-		System.out.println(a1);
-		
-		short a2=32767;
-		System.out.println(a2);
-		
-		int a3=2345678;
-		System.out.println(a3);
-		
-		a3=a2;
-		System.out.println(a3);
-		
-		long a4=1234567;
-		System.out.println(a4);
-		
-		a4=1234567890456l;
-		System.out.println(a4);
-		
-		float a5=3456789.8145f;
-		System.out.println(a5);
-		
-		double a6=45678.1234567d;
-		System.out.println(a6);
-		
-		char a7='a';
-		System.out.println(a7);
-		
-		String a8="234567812sdrt!@#%";
-		System.out.println(a8);
-		
-	    }
 	
-}
+	public static void main(String[] args) {
+	 boolean a=true;
+	 System.out.println(a);
+	 a=false;
+	 System.out.println(a);
+	 byte a1=125;
+	 System.out.println(a1);
+		
+	 short a2=32767;
+	 System.out.println(a2);
+		
+	 int a3=2345678;
+	 System.out.println(a3);
+		
+	 a3=a2;
+	 System.out.println(a3);
+		
+	 long a4=1234567;
+	 System.out.println(a4);
+		
+	 a4=1234567890456l;
+	 System.out.println(a4);
+		
+	 float a5=3456789.8145f;
+	 System.out.println(a5);
+		
+	 double a6=45678.1234567d;
+	 System.out.println(a6);
+		
+	 char a7='a';
+	 System.out.println(a7);
+		
+	 String a8="234567812sdrt!@#%";
+	 System.out.println(a8);
+	 
+	}
+	
+  {
+
+	
+
 				
 		
 				
