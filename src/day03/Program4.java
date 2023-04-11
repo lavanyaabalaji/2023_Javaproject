9e401d4f233eb46b9a677973a5fad0df51985c04
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-11 19:15:04 +0530

    Day03
    Mainly commit static/non-static

---
+++ src/day03/Program4.java
@@ -0,0 +1,18 @@
+package day03;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		int x=addFunction(10,20);
+		System.out.println(x);
+		System.out.println(x*x);
+
+	}
+	
+	public static int addFunction(int a,intb)
+	{
+		return a+b;
+		
+	}
+
+}
