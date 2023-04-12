36401dbba5f4fad79edb18de85e00376b0bd801a
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-12 20:51:08 +0530

    day04.packageB
    Inheritance. static and non-static

--- src/day04/packageB/Program3.java
+++ src/day04/packageB/Program3.java
@@ -1,9 +1,16 @@
 package day04.packageB;
 
-public class Program3 {
+import day04.packageA.Program1;
+
+
+public class Program3 extends Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		addFunction_static(10,20);
+		mulFunction_static(20,10);
+		Program3 x=new Program3();
+		x.addFunction(10,20);
+		x.mulFunction(20, 10);
 
 	}
 
--- src/day04/packageB/Program4.java
+++ src/day04/packageB/Program4.java
@@ -1,9 +1,13 @@
 package day04.packageB;
+import day04.packageA.Program1;
+
 
 public class Program4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Program1 x=new Program1();
+		x.addFunction(10, 20);
+		x.addFunction_static(20,30);
 
 	}
 
