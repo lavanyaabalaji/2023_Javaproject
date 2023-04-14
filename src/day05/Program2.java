6d93f2bc30d68ef1aa42ada511586ee87468280f
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-14 18:48:12 +0530

    day05
    Method overloading

--- src/day05/Program2.java
+++ src/day05/Program2.java
@@ -1,28 +1,43 @@
 package day05;
 
-public class Program2 {
-
+public class Program2{
 	public static void main(String[] args) {
-		add(1,2,3);
-		add(2,3);
-		add(1.1,2.2,3.3);
-		add(2.3,3.4);
-		}
+		
+		add(2d,3l);//d,d
+		add(3,2f);//f,f
+		add(2f,2l);//f,f
+		add(3d,3f);//d,d
+	}
 	
-	public static void add(int a,int b)
-	{System.out.println(a+b);
-	}
-	public static void(int a,int b,int c)
-	{
-		System.out.println(a+b+c);
-	}
-	public static void add(double a,double b)
-	{
-		System.out.println(a+b);
-	}
-	public static void add(double a,double b,double c)
-	{
-		System.out.println(a+b+c);
-	}
-
+ public static void add(byte a,byte b)
+ {
+	 System.out.println(a+b);
+ }
+ 
+ public static void add(short a,short b)
+ {
+	 System.out.println(a+b);
+ }
+ 
+ public static void add(int a,int b)
+ {
+	 System.out.println(a+b);
+ }
+ public static void add(long a,long b)
+ {
+	 System.out.println(a+b);
+ }
+ public static void add(float a,float b)
+ {
+	 System.out.println(a+b);
+ }
+ public static void add(double a,double b)
+ {
+	 System.out.println(a+b);
+ }
+ 
 }
+
+		
+
+
