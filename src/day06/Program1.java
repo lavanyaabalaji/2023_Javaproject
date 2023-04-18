ccd42e23a9ea0315528961d5fc9bf6af3fd2c163
lavanyaabalaji <rlavanyaabalaji@gmail.com>
2023-04-18 20:12:25 +0530

    day06
    Overriding

---
+++ src/day06/Program1.java
@@ -0,0 +1,15 @@
+package day06;
+
+public class Program1 {
+
+	protected  void cal(int a,int b) { 
+		System.out.println("Program1- Calculator");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+
+	}
+
+}
